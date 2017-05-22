5077e5118624146ec153851fe7964a9a72edd919
Ben Sharafian <benms64@gmail.com>
2017-05-22 18:32:38 +0200

    feat: pull in useful functionality from plugin virtual

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  Balance: require('./src/balance'),
+  Errors: require('./src/errors'),
+  HttpRpc: require('./src/rpc'),
+  TransferLog: require('./src/transferlog'),
+  Validator: require('./src/validator')
+}
---
+++ src/validator.js
@@ -0,0 +1,191 @@
+'use strict'
+const BigNumber = require('bignumber.js')
+const InvalidFieldsError = require('./errors').InvalidFieldsError
+const util = require('util')
+
+const REGEX_32_BYTES_AS_BASE64URL = /^[A-Za-z0-9_-]{43}$/
+const REGEX_INTEGER = /
+const xor = (a, b) => ((a || b) && (!a || !b))
+const omitUndefined = (o) => Object.keys(o).reduce((a, e) => {
+  if (o[e] !== undefined) a[e] = o[e]
+  return a
+}, {})
+
+module.exports = class Validator {
+  constructor (plugin) {
+    this._plugin = plugin
+  }
+
+  normalizeOutgoingTransfer (t) {
+    validateOutgoingTransfer(t)
+    return omitUndefined({
+      id: t.id,
+      to: t.to || t.account,
+      from: this._plugin.getAccount(),
+      ledger: this._plugin.getInfo().prefix,
+      ilp: t.ilp,
+      executionCondition: t.executionCondition,
+      expiresAt: t.expiresAt,
+      custom: t.custom,
+      noteToSelf: t.noteToSelf
+    })
+  }
+
+  normalizeIncomingTransfer (t) {
+    validateIncomingTransfer(t)
+    return omitUndefined({
+      id: t.id,
+      to: t.to,
+      from: t.from,
+      ledger: this._plugin.getInfo().prefix,
+      ilp: t.ilp,
+      executionCondition: t.executionCondition,
+      expiresAt: t.expiresAt,
+      custom: t.custom
+    })
+  }
+
+  normalizeOutgoingMessage (m) {
+    validateOutgoingMessage(m)
+  }
+
+  normalizeIncomingMessage (m) {
+
+  }
+
+  validateIncomingTransfer (t) {
+    this.validateTransfer(t)
+    if (t.account) return
+    this.assertIncoming(t)
+  }
+
+  validateOutgoingTransfer (t) {
+    this.validateTransfer(t)
+    if (t.account) return
+    this.assertOutgoing(t)
+  }
+
+  validateTransfer (t) {
+    assert(t.id, 'must have an id')
+    assert(t.ledger, 'must have a ledger')
+    assert(t.amount, 'must have an amount')
+
+    assertString(t.id, 'id')
+    assertNumber(t.amount, 'amount')
+    assertObject(t.data, 'data')
+    assertObject(t.noteToSelf, 'noteToSelf')
+    assertObject(t.custom, 'custom')
+    assertConditionOrPreimage(t.executionCondition, 'executionCondition')
+    assertString(t.expiresAt, 'expiresAt')
+    assertPrefix(t.ledger, this._plugin.getInfo().prefix, 'ledger')
+
+    if (xor(t.executionCondition, t.expiresAt)) {
+      throw new Error('executionCondition (' + t.executionCondition +
+        ') and expiresAt (' + t.expiresAt +
+        ') must both be set if either is set')
+    }
+
+    if (t.account) {
+      util.deprecate(() => {}, 'switch from the "account" field to the "to" and "from" fields!')()
+      assertString(t.account, 'account')
+      return
+    }
+
+    assert(t.to, 'must have a destination (.to)')
+  }
+
+  validateIncomingMessage (m) {
+    this.validateMessage(m)
+    if (m.account) return
+    this.assertIncoming(m)
+  }
+
+  validateOutgoingMessage (m) {
+    this.validateMessage(m)
+    if (m.account) return
+    this.assertOutgoing(m)
+  }
+
+  validateMessage (m) {
+    if (m.ilp) {
+      assertString(m.ilp, 'message ilp must be a string')
+    }
+
+    if (m.account) {
+      util.deprecate(() => {}, 'switch from the "account" field to the "to" and "from" fields!')()
+      assertString(m.account, 'account')
+      return
+    }
+
+    assert(m.to, 'must have a destination (.to)')
+    assertPrefix(m.ledger, this._prefix, 'ledger')
+  }
+
+  validateFulfillment (f) {
+    assert(f, 'fulfillment must not be "' + f + '"')
+    assertConditionOrPreimage(f, 'fulfillment')
+  }
+
+  assertIncoming (o) {
+    assertAccount(o.to, this._account, 'to')
+    assertAccount(o.from, this._peer, 'from')
+  }
+
+  assertOutgoing (o) {
+    assertAccount(o.to, this._peer, 'to')
+    if (o.from) assertAccount(o.from, this._account, 'from')
+  }
+
+}
+
+function assert (cond, msg) {
+  if (!cond) throw new InvalidFieldsError(msg)
+}
+
+function assertType (value, name, type) {
+  assert(!value || typeof (value) === type,
+    name + ' (' + value + ') must be a non-empty ' + type)
+}
+
+function assertString (value, name) {
+  assertType(value, name, 'string')
+}
+
+function assertObject (value, name) {
+  assertType(value, name, 'object')
+}
+
+function assertPrefix (value, prefix, name) {
+  assertString(value, name)
+  assert(value === prefix,
+    name + ' (' + value + ') must match ILP prefix: ' + prefix)
+}
+
+function assertAccount (value, account, name) {
+  assertString(value, name)
+  assert(value === account,
+    name + ' (' + value + ') must match account: ' + account)
+}
+
+function assertConditionOrPreimage (value, name) {
+  if (!value) return
+  assertString(value, name)
+  if (!REGEX_32_BYTES_AS_BASE64URL.test(value)) {
+    throw new InvalidFieldsError(name + ' (' + value + '): Not a valid 32-byte base64url encoded string')
+  }
+}
+
+function isNumber (number) {
+  try {
+    return !!(new BigNumber(number))
+  } catch (e) {
+    return false
+  }
+}
+
+function assertNumber (value, name) {
+  assert(isNumber(value),
+    name + ' (' + value + ') must be a number')
+  assert((new BigNumber(value)).gt(new BigNumber('0')),
+    name + ' (' + value + ') must be positive')
+}
